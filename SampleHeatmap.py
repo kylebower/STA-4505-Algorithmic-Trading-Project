aac002a821e23e5f55f8135773f2bfb8dc76c511
Kyle Bower <kylewbower@gmail.com>
2020-05-20 22:11:23 -0400

    Update SampleHeatmap.py

--- SampleHeatmap.py
+++ SampleHeatmap.py
@@ -1,5 +1,5 @@
 import numpy as np
-import seaborn as sb
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Get a data set of optimal actions for pairs (inventory,price)
@@ -10,8 +10,8 @@ print(data)
 
 # Columns are x axis (inventory) and rows are y axis (price)
 
-cmap = sb.diverging_palette(20, 220, as_cmap=True)
-heat_map = sb.heatmap(data, cmap=cmap)
+cmap = sns.diverging_palette(20, 240, as_cmap=True)  # husl color system
+heat_map = sns.heatmap(data, cmap=cmap)
 plt.xlabel("Inventory")
 plt.ylabel("Price")
 plt.show()
