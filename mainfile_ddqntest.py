5f3489e2ff2a6b7841604b053d7741db47f5e217
stevenacampbell <64761520+stevenacampbell@users.noreply.github.com>
2020-07-06 18:15:07 -0400

    Create mainfile_ddqntest.py

---
+++ mainfile_ddqntest.py
@@ -0,0 +1,36 @@
+import gym
+from DDQNetwork_OptimalExecution import Agent
+from utils import plot_learning_curve
+import numpy as np
+
+if __name__ == '__main__':
+    env = gym.make('LunarLander-v2')
+    agent = Agent(gamma=0.99, epsilon=1.0, lr=0.001, n_actions=4, input_dims=[8], mem_size=10000,
+                  batch_size=64, eps_min = 0.01, eps_dec = 5e-5, replace=1000, fc1_dims=64,
+                  fc2_dims=64)
+    scores, eps_history = [], []
+    n_games = 100
+
+    for i in range(n_games):
+        score = 0
+        done = False
+        observation = env.reset()
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            agent.store_transition(observation, action, reward,
+                                   observation_, done)
+            agent.learn()
+            observation = observation_
+        scores.append(score)
+        eps_history.append(agent.epsilon)
+
+        avg_score = np.mean(scores[-100:])
+
+        print('episode ', i, 'score %.2f' % score,
+              'average score %.2f' % avg_score,
+              'epsilon %.2f' % agent.epsilon)
+    x = [i + 1 for i in range(n_games)]
+    filename = 'lunar_lander_ddqn.png'
+    plot_learning_curve(x, scores, eps_history, filename)
