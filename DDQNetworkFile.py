37559873da20a9c0fc7076339e2a043407965136
stevenacampbell <64761520+stevenacampbell@users.noreply.github.com>
2020-07-06 18:14:10 -0400

    Update DDQNetworkFile.py

--- DDQNetworkFile.py
+++ DDQNetworkFile.py
@@ -46,7 +46,7 @@ class ReplayBuffer():
                                          dtype=np.float32)
         self.action_memory = np.zeros(self.mem_size, dtype=np.int64)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.uint8)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
 
     def store_transition(self, state, action, reward, state_, done):
         index = self.mem_cntr % self.mem_size
