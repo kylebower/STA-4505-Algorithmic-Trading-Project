271b4bace164d8d6f6c06c6a52c94d4f900d53bf
Kyle Bower <kylewbower@gmail.com>
2020-07-09 16:11:43 -0400

    cleaned up code

--- mainfile_ddqntest.py
+++ mainfile_ddqntest.py
@@ -1,12 +1,13 @@
 import gym
-from DDQNetwork_OptimalExecution import Agent
+# from DDQNetwork_OptimalExecution import Agent
+from DDQNetworkFile import Agent
 from utils import plot_learning_curve
 import numpy as np
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
     agent = Agent(gamma=0.99, epsilon=1.0, lr=0.001, n_actions=4, input_dims=[8], mem_size=10000,
-                  batch_size=64, eps_min = 0.01, eps_dec = 5e-6, replace=1000, fc1_dims=64,
+                  batch_size=64, eps_min=0.01, eps_dec=5e-6, replace=1000, fc1_dims=64,
                   fc2_dims=64)
     scores, eps_history = [], []
     n_games = 500
@@ -28,7 +29,7 @@ if __name__ == '__main__':
 
         avg_score = np.mean(scores[-100:])
 
-        print('episode ', i+1 , 'score %.2f' % score,
+        print('episode ', i+1, 'score %.2f' % score,
               'average score %.2f' % avg_score,
               'epsilon %.2f' % agent.epsilon)
     x = [i + 1 for i in range(n_games)]
